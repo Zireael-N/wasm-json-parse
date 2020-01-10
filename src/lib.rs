77601587c8d6777b46aa9dd6ee01de7675c8af1c
Zireael <13710677+Zireael-N@users.noreply.github.com>
2020-01-10 20:35:29 +0300

    Use Vec::set_len() instead of a custom container

--- src/lib.rs
+++ src/lib.rs
@@ -56,59 +56,30 @@ pub fn parse_json_typed(data: &str) -> u8 {
 }
 
 // Warning: unsafe code ahead
-static mut DATA: Option<SliceContainer> = None;
-
-#[derive(Debug)]
-struct SliceContainer<'a> {
-    slice: &'a mut [u8],
-    layout: std::alloc::Layout,
-}
-
-impl<'a> SliceContainer<'a> {
-    unsafe fn realloc(&mut self, new_size: usize) {
-        let ptr = std::alloc::realloc(self.slice.as_mut_ptr(), self.layout, new_size);
-        self.slice = std::slice::from_raw_parts_mut(ptr, new_size);
-    }
-}
-
-/**
-  * Apparently converting std::slice::from_raw_parts
-  * into a Box does not deallocate memory when the Box is dropped.
-  */
-impl<'a> Drop for SliceContainer<'a> {
-    fn drop(&mut self) {
-        unsafe { std::alloc::dealloc(self.slice.as_mut_ptr(), self.layout) };
-    }
-}
+static mut DATA: Option<Vec<u8>> = None;
 
 #[wasm_bindgen]
 pub fn allocate_buffer(size: usize) -> *const u8 {
     unsafe {
-        if let Some(v) = &mut DATA {
-            v.realloc(size);
-        } else {
-            let align = std::mem::align_of::<u8>();
-            let layout = std::alloc::Layout::from_size_align(size, align).unwrap();
-            let ptr = std::alloc::alloc(layout);
-            DATA = Some(SliceContainer {
-                slice: std::slice::from_raw_parts_mut(ptr, size),
-                layout,
-            });
-        }
-        DATA.as_ref().unwrap().slice.as_ptr()
+        DATA = None; // Dealloc old Vec
+
+        let mut v = Vec::with_capacity(size);
+        v.set_len(size);
+        DATA = Some(v);
+        DATA.as_ref().unwrap().as_ptr()
     }
 }
 
 #[wasm_bindgen]
 pub fn parse_json_move() -> u8 {
     serde_json::from_str::<Value>(
-        unsafe { &std::str::from_utf8_unchecked(DATA.as_ref().unwrap().slice) }
+        unsafe { &std::str::from_utf8_unchecked(DATA.as_ref().unwrap()) }
     ).as_u8()
 }
 
 #[wasm_bindgen]
 pub fn parse_json_move_typed() -> u8 {
     serde_json::from_str::<Canada>(
-        unsafe { &std::str::from_utf8_unchecked(DATA.as_ref().unwrap().slice) }
+        unsafe { &std::str::from_utf8_unchecked(DATA.as_ref().unwrap()) }
     ).as_u8()
 }
